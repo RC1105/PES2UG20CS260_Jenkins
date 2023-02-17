55c68d8b9c1bb977d851185a98589639421009f2
Raghav Chugh <92471414+RC1105@users.noreply.github.com>
2023-02-17 18:49:20 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,6 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
-  cout<< "New changes added by PES2UG20CS260"
+  cout<< "New changes added by PES2UG20CS260";
   return 0;
 }
